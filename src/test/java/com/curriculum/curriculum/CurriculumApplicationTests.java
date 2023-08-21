c33a9ccf7a2f3e0e63d1a094775908eada36fb6b
daosph <davidhernandez9801@gmail.com>
2023-08-20 23:30:24 -0500

    primer commit

---
+++ src/test/java/com/curriculum/curriculum/CurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curriculum.curriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
